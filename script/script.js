db7c9d74a4c70ab1a0a01cfde38165a5601db8ee
DmitryKress <dmitry_job@bk.ru>
2019-10-05 17:01:38 +0300

    1

---
+++ script/script.js
@@ -0,0 +1,9 @@
+var subject = ["HTML", 11, "JavaScript", 2, "CSS", 23, 12];
+var subject1 = subject.push("-5");
+subject.unshift("1000");
+var subject1 = subject.push("Last array element");
+var subject2 = subject.splice(3, 1);
+var subject3 = subject.filter(callback[thisArg]);
+
+console.log(subject.length);
+console.log(subject);
